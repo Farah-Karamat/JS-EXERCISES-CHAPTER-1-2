b37101ae5a5868d8f65589c76d2c79f4b9d94aca
Farah Karamat <farahkaramat@gmail.com>
2024-09-30 22:34:57 +0500

    ASSIGNMENT-1 & 2
    EXERCISES-CHAPTER-1-2

---
+++ EXERCISES JAVASCRIPT/JS-EXERCISE-1-CHAPTER-1-ALERT/app.js
@@ -0,0 +1,35 @@
+/*Q.1. What does the alert function do in JavaScript?
+//Answer:
+The alert() function in JavaScript displays a popup message box with a specified message and an "OK" button. It is used to show information or alerts to the user.
+
+ //================================================================================================================================================================
+ 
+ /*Q.2.Write the code to display an alert with the message "Welcome to my website!"*/
+ //Answer:
+ //alert(" Welcome to my website! Enjoy your stay.");
+
+ //================================================================================================================================================================
+
+/*Q.3. What happens if you capitalize the alert keyword?
+//Answer:
+If we capitalize the "alert" keyword with "Alert", it will resultantly show error because Javascript is case-sensitive.
+
+//=================================================================================================================================================================
+
+Q.4. Write the correct JavaScript syntax to display "Hello, world!" in an alert box.*/
+//Answer:
+//alert("Hello, World!");
+
+//==================================================================================================================================================================
+
+/*Q.5. Write a JavaScript alert statement with proper formatting and explain each part of the
+code.*/
+//Answer:
+// alert("Welcome to the City of Lights!");
+// a. alert: This is the JavaScript function used to display a message to the user in a dialog box.
+// b. (): The round brackets are used to hold the argument or message.
+// c. "" :The String; Any message or argument that will be displayed to the user is hold under these quotes.
+// d. ; : End of command or statement. It's optional but to keep this practice is good in Javascript.
+
+//==================================END OF EXERCISE-1==================================================================================================================================
+
---
+++ EXERCISES JAVASCRIPT/JS-EXERCISE-2-CHAPTER-2-VARIABLES FOR STRINGS/app.js
@@ -0,0 +1,48 @@
+/*Q.1.Write code that declares a variable message, assigns the value "Hello, world!" to
+it, and then displays an alert with that message*/
+//Answer:
+// var message = "Hello, world!";
+// alert(message);
+
+//========================================================================================================================
+
+/*Q.2.Declare a variable named age, leave it undefined, then assign the value 25 to it in a
+later statement. Display an alert showing the value of age.*/
+//Answer:
+// var age;        
+// age = 25;       
+// alert(age);  
+
+//========================================================================================================================
+
+/*Q.3. Declare a variable named greeting, assign it the value "Good morning!", and then
+display it in an alert. Afterwards, change the value of greeting to "Good evening!"
+and display the new value in another alert.*/
+//Answer:
+// var greetings;
+// greetings = "Good Morning!";
+// alert(greetings);
+// greetings = "Good Evening!";
+// alert(greetings);
+
+//=========================================================================================================================
+
+/*Q.4. Create a variable called favoriteColor and assign it the value "blue". Then display
+an alert that says "My favorite color is blue" using the variable.*/
+//Answer:
+// var favoriteColor;
+// favoriteColor = "blue";
+// alert("My favorite color is " + favoriteColor);
+
+//=========================================================================================================================
+
+/*Q.5. Declare a variable named age, leave it undefined, then assign the value 25 to it in a
+later statement. Display an alert showing the value of age.*/
+//Answer:
+//(Question No:2 & 5 are same.)
+// var age;        
+// age = 25;       
+// alert(age);  
+
+//===================================END OF EXERCISE-2 ====================================================================
+
